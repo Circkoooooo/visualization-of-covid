3476951dd0f5921cb8171ee03f836b54ccd94f0c
2389451262@qq.com <2389451262@qq.com>
2022-08-11 17:58:32 +0800

    init

---
+++ src/pages/Home.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+ref()
+</script>
+<template>
+
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		vue(),
+		AutoImport({
+			include: [/\.vue$/, /\.vue\?vue/],
+			imports: ['vue', 'vue-router'],
+			dts: './auto-imports.d.ts',
+		}),
+		Components({
+			extensions: ['vue'],
+		}),
+	],
+});
